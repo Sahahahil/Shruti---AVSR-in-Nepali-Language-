19db81a1fc30a79c3e85df07e9b9357e10715751
Hasana Manandhar <manandharhasana07@gmail.com>
2025-10-08 18:00:55 +0545

    Homepage layout done

--- frontend/nepali-avsr-frontend/src/components/Hero.tsx
+++ frontend/nepali-avsr-frontend/src/components/Hero.tsx
@@ -1,20 +1,35 @@
 "use client";
 import { useState } from "react";
 
+
 import GetStartedModal from "./GetStartedModal";
 import "@/styles/components/_hero.scss"
 
 export default function Hero(){
-    const[showPopUp, setShowPopUp]=useState(false);
-    return(
-        <section className="hero">
-            <div className="hero-content">
-                <h1>Nepali Audio-Video Speech Recognition</h1>
-                <p> Experience real-time and offline Nepali speech recognition using
-                cutting-edge AI models.</p>
-                <button onClick={()=>setShowPopUp(true)}> Get Started </button>
-            </div>
-            {showPopUp&& <GetStartedModal onClose={()=> setShowPopUp(false)}></GetStartedModal>}
-        </section>
-    )
+    const[showPopup, setShowPopup]=useState(false);
+    return (
+    <section className="hero">
+      {/* Background Video */}
+      <video className="hero-video" autoPlay muted loop>
+        <source src="/videos/hero.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+
+      {/* Dark Overlay */}
+      <div className="hero-overlay"></div>
+
+      {/* Overlay Content */}
+      <div className="hero-content">
+        <h1>Nepali Audio-Visual Speech Recognition</h1>
+        <p>
+          Experience real-time and offline Nepali speech recognition using
+          cutting-edge AI models.
+        </p>
+        <button onClick={() => setShowPopup(true)}>Get Started</button>
+      </div>
+
+      {showPopup && <GetStartedModal onClose={() => setShowPopup(false)} />}
+    </section>
+  );
 }
